e7b9dd6df716874eb364e9ebe07330d704728947
PabloMatilla <al415550@uji.es>
2024-02-29 22:18:21 +0100

    Creación código simulación parrot

--- test_ws/src/drone/src/simulacion/src/prueba_simulacion.py
+++ test_ws/src/drone/src/simulacion/src/prueba_simulacion.py
@@ -26,11 +26,12 @@ def image_callback(msg):
     except CvBridgeError as e:
         print(e)
     else:
-        cv2.imshow('cv_img', image_np)
-        cv2.waitKey(5)
+        pass
+        #cv2.imshow('cv_img', image_np)
+        #cv2.waitKey(5)
 
 def pose_callback(msg):
-     print(msg)
+     #print(msg)
      global x, y, z
      x = msg.position.x
      y = msg.position.y
@@ -38,12 +39,38 @@ def pose_callback(msg):
 
 
 def main():
-
-    global x, y , z
+    global x, y, z
 
     rospy.init_node('Prueba_drone')
     print(".. Nodo inicializado ...................................... NI")
 
+    print("Nodo publica a .. .. ..                                     NP")
+    takeof_topic = '/bebop/takeoff'
+    pub_takeof = rospy.Publisher(takeof_topic, Empty, queue_size=10)
+    print("\t\t", takeof_topic)
+
+    land_topic = '/bebop/land'
+    pub_land = rospy.Publisher(land_topic, Empty, queue_size=10)
+    print("\t\t", land_topic)
+
+    vel_topic = '/bebop/cmd_vel'
+    pub_vel = rospy.Publisher(vel_topic, Twist, queue_size=10)
+    print("\t\t", vel_topic)
+
+    rate = rospy.Rate(30)
+
+    # Inicializar un mensaje Empty
+    takeoff_msg = Empty()
+
+    # Publicar el mensaje de despegue una sola vez
+    pub_takeof.publish(takeoff_msg)
+    
+    print("Despega")
+    # Esperar un período de 3 seg
+    rospy.sleep(30)
+    print("YA")
+
+    # Suscripciones a tópicos
     image_topic = '/bebop2/camera_base/image_raw/compressed'
     rospy.Subscriber(image_topic, CompressedImage, image_callback)
     print("Nodo suscrito a . . . .                                     NS")
@@ -54,44 +81,17 @@ def main():
     print("Nodo suscrito a . . . .                                     NS")
     print("\t\t", pose_topic)
 
-    print("Nodo publica a .. .. ..                                     NP")
-
-    takeof_topic = '/bebop2/takeoff'
-    pub_takeof = rospy.Publisher(takeof_topic, Empty, queue_size=10)
-    print("\t\t", takeof_topic)
-
-    land_topic = '/bebop2/land'
-    pub_land = rospy.Publisher(land_topic, Empty, queue_size=10)
-    print("\t\t", land_topic)
-
-    vel_topic = '/bebop2/cmd_vel'
-    pub_vel = rospy.Publisher(vel_topic, Twist, queue_size=10)
-    print("\t\t", vel_topic)
-    
-    rate = rospy.Rate(30)
-
-
-        # Inicializar un mensaje Empty
-    takeoff_msg = Empty()
-
-        # Publicar el mensaje una sola vez
-    pub_takeof.publish(takeoff_msg)
-
-    # Esperar un período de 3 seg 
-    rospy.sleep(3)
 
     twist_msg = Twist()
 
     while (z < 5 and not rospy.is_shutdown()):
-        twist_msg.linear.z = 0.5 
+        twist_msg.linear.z = 0.5
 
         # Publicar el mensaje Twist
         pub_vel.publish(twist_msg)
         rospy.sleep(0.2)
 
         rate.sleep()
-
-    
 
 if __name__ == "__main__":
     main()

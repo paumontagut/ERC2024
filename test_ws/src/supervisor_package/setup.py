8ee6a9e5915aae8d6d809bd1fddd6cc7158897f0
diegomarzaa <al426641@uji.es>
2024-12-08 18:45:12 +0100

    path bien

--- test_ws/src/supervisor_package/setup.py
+++ test_ws/src/supervisor_package/setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+import os
+from glob import glob
 
 package_name = 'supervisor_package'
 
@@ -10,7 +12,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name, ['config/topics_programas.py']),
+        (os.path.join('share', package_name, 'config'), glob('supervisor/config/*.py')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
